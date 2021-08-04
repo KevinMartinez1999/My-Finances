c26e398a19bc5ccebf973c7ece219486fda2882f
KevinMartinez1999 <kevin.martinez1@udea.edu.co>
2021-08-04 14:34:40 -0500

    Base del proyecto.
    Signed-off-by: KevinMartinez1999 <kevin.martinez1@udea.edu.co>

---
+++ app/src/main/java/com/example/myfinances/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.example.myfinances
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Window
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+import kotlin.concurrent.timerTask
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        this.supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
+        setContentView(R.layout.activity_splash)
+        val timer = Timer()
+        timer.schedule(timerTask { goToLoginActivity() }, 1500)
+    }
+
+    private fun goToLoginActivity() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
